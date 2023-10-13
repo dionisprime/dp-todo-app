6b73d60f5cab8ecfe0ba596bd2c29f96710e8f80
Dionis Prime <dioniskruspe@gmail.com>
2023-10-13 15:45:02 +0300

    Добавил пакет dotenv для работы с переменными окружения и файлом .env

--- todo_v6-process.js
+++ todo_v6-process.js
@@ -1,86 +1,87 @@
-const { simpleShowList } = require("./functions/showFunctions.js");
-const todoListCreate = require("./functions/todoListCreate.js");
+require('dotenv').config();
+const { simpleShowList } = require('./functions/showFunctions.js');
+const todoListCreate = require('./functions/todoListCreate.js');
 const {
     createTaskFromJson,
     deleteTask,
     changeStatus,
     changePriority,
-} = require("./functions/editFunctions.js");
-const list = require("./todoList.json");
+} = require('./functions/editFunctions.js');
+const list = require('./todoList.json');
 
 const myTodoList = todoListCreate(list);
 
 //------------------------------------------
 // DB Connect
-const url = "mongodb://127.0.0.1:27017/test1"; // урл для сервиса с mongodb
-const { MongoClient } = require("mongodb"); // конструктор клиентов mongodb
+const url = process.env.DB_CONNECTION_URL; // урл для сервиса с mongodb
+const { MongoClient } = require('mongodb'); // конструктор клиентов mongodb
 const client = new MongoClient(url); // создаем новый клиент для работы с базой
 (async function () {
     try {
         await client.connect(); // подключаемся к базе
     } catch (error) {
-        console.log("Не удалось подключиться к MongoDB:", error.message);
+        console.log('Не удалось подключиться к MongoDB:', error.message);
     }
 })();
 
 //------------------------------------------
 
 //------------------------------------------
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 //------------------------------------------
 // для обработки CORS
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
     next();
 });
 //------------------------------------------
 app.use(bodyParser.json());
-app.get("/", (req, res) => {
-    res.send("Привет! По пути /tasks будет список задач!)");
+app.get('/', (req, res) => {
+    res.send('Привет! По пути /tasks будет список задач!)');
 });
 
-app.get("/tasks", async (req, res) => {
+app.get('/tasks', async (req, res) => {
     try {
         const allTasksFromDB = await client
-            .db("todo-mongo-db")
-            .collection("tasks")
+            .db('todo-mongo-db')
+            .collection('tasks')
             .find({})
             .toArray();
         console.log(allTasksFromDB);
         res.send(allTasksFromDB);
     } catch (error) {
-        console.log("Ошибка при получении задач из MongoDB:", error.message);
-        res.status(500).send("Ошибка при получении задач");
+        console.log('Ошибка при получении задач из MongoDB:', error.message);
+        res.status(500).send('Ошибка при получении задач');
     }
 });
 
-app.post("/tasks", async (req, res) => {
+app.post('/tasks', async (req, res) => {
     const jsonTask = req.body;
     const message = createTaskFromJson(myTodoList, jsonTask);
     try {
         await client
-            .db("todo-mongo-db")
-            .collection("tasks")
+            .db('todo-mongo-db')
+            .collection('tasks')
             .insertOne(jsonTask);
     } catch (error) {
-        console.log("Не удалось добавить задачу в MongoDB", error.message);
+        console.log('Не удалось добавить задачу в MongoDB', error.message);
     }
     res.send(message);
 });
 
-app.put("/tasks/:name/status/:status", async (req, res) => {
+app.put('/tasks/:name/status/:status', async (req, res) => {
     const taskName = req.params.name;
     const taskStatus = req.params.status;
     try {
         const result = await client
-            .db("todo-mongo-db")
-            .collection("tasks")
+            .db('todo-mongo-db')
+            .collection('tasks')
             .updateOne(
                 { name: taskName },
                 {
@@ -99,13 +100,13 @@ app.put("/tasks/:name/status/:status", async (req, res) => {
     }
 });
 
-app.put("/tasks/:name/priority/:priority", async (req, res) => {
+app.put('/tasks/:name/priority/:priority', async (req, res) => {
     const taskName = req.params.name;
     const taskPriority = req.params.priority;
     try {
         const result = await client
-            .db("todo-mongo-db")
-            .collection("tasks")
+            .db('todo-mongo-db')
+            .collection('tasks')
             .updateOne(
                 { name: taskName },
                 {
@@ -126,12 +127,12 @@ app.put("/tasks/:name/priority/:priority", async (req, res) => {
     }
 });
 
-app.delete("/tasks/:name", async (req, res) => {
+app.delete('/tasks/:name', async (req, res) => {
     const taskName = req.params.name;
     try {
         const result = await client
-            .db("todo-mongo-db")
-            .collection("tasks")
+            .db('todo-mongo-db')
+            .collection('tasks')
             .deleteOne({ name: taskName });
         if (result.deletedCount === 1) {
             res.send(`Задача "${taskName}" успешно удалена`);
@@ -139,7 +140,7 @@ app.delete("/tasks/:name", async (req, res) => {
             res.send(`Задача "${taskName}" не найдена`);
         }
     } catch (error) {
-        console.log("Не удалось удалить задачу из MongoDB", error.message);
+        console.log('Не удалось удалить задачу из MongoDB', error.message);
         res.send(`Не удалось удалить задачу "${taskName}"`);
     }
 });

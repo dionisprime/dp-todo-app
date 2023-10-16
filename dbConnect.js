f22e41fc1880db9f7c55905a71b13e6c9b72f30a
Dionis Prime <dioniskruspe@gmail.com>
2023-10-16 17:38:50 +0300

    Переписал коннект к базе с использованием mongoose, добавил модель задачи, вынес модель и подключение к БД в отдельные модули реализовал GET запрос на добавление задачи, получения всех задач и конкретной по айди а так же дополнительно, удаление и изменение задач

--- dbConnect.js
+++ dbConnect.js
@@ -1,6 +1,6 @@
 // DB Connect
-const { MongoClient } = require("mongodb"); // конструктор клиентов mongodb
+const mongoose = require('mongoose');
 const url = process.env.DB_CONNECTION_URL; // урл для сервиса с mongodb
-const client = new MongoClient(url); // создаем новый клиент для работы с базой
-client.connect(); // подключаемся к базе
-module.exports = client;
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+module.exports = mongoose;
--- todo_v8_odm.js
+++ todo_v8_odm.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const { ObjectId } = require('mongodb');
-const client = require('./dbConnect.js');
+const Task = require('./TaskModel.js');
 const express = require('express');
+//------------------------------------------
 const port = process.env.PORT;
 const app = express();
-
 //------------------------------------------
 // для обработки CORS
 app.use((req, res, next) => {
@@ -15,6 +15,7 @@ app.use((req, res, next) => {
     next();
 });
 //------------------------------------------
+
 app.use(express.json()); // бодипарсер
 
 app.get('/', (req, res) => {
@@ -22,16 +23,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/tasks/:taskId', async (req, res) => {
+    // получение задачи из БД по id
     const taskId = req.params.taskId;
     try {
-        const result = await client
-            .db('todo-mongo-db')
-            .collection('tasks')
-            .findOne({
-                _id: new ObjectId(taskId),
-            });
-        console.log(result);
-        res.send(result);
+        const task = await Task.findById(taskId);
+        if (!task) {
+            console.log('нет такой задачи');
+            res.send('нет такой задачи');
+        }
+        if (task) {
+            res.json(task);
+        }
     } catch (error) {
         console.log('Ошибка при получении задачи из MongoDB:', error.message);
         res.status(500).send('Ошибка при получении задачи');
@@ -39,27 +41,32 @@ app.get('/tasks/:taskId', async (req, res) => {
 });
 
 app.get('/tasks', async (req, res) => {
+    // Получаем все таски
     try {
-        const allTasksFromDB = await client
-            .db('todo-mongo-db')
-            .collection('tasks')
-            .find({})
-            .toArray();
-        res.send(allTasksFromDB);
+        const tasks = await Task.find({});
+
+        if (tasks) {
+            res.status(200).json(tasks);
+        } else {
+            res.status(404).json({ error: 'Задача не найдена' });
+        }
     } catch (error) {
-        console.log('Ошибка при получении задач из MongoDB:', error.message);
-        res.status(500).send('Ошибка при получении задач');
+        console.error('Ошибка при получении задачи:', error);
+        res.status(500).json({ error: 'Ошибка сервера' });
     }
 });
 
 app.post('/tasks', async (req, res) => {
-    const jsonTask = req.body;
+    // создание задачи из тела запроса и добавление в БД
+    const { name, status, priority } = req.body;
+
     try {
-        await client
-            .db('todo-mongo-db')
-            .collection('tasks')
-            .insertOne(jsonTask);
-        res.send('Добавляем таску');
+        const task = await Task.create({
+            name: name,
+            status: status,
+            priority: priority,
+        });
+        res.send(task);
     } catch (error) {
         console.log('Не удалось добавить задачу в MongoDB', error.message);
         res.send('Не удалось добавить задачу в MongoDB', error.message);
@@ -67,63 +74,64 @@ app.post('/tasks', async (req, res) => {
 });
 
 app.put('/tasks/:taskId/status/:status', async (req, res) => {
+    // изменение статуса задачи по айди
     const taskId = req.params.taskId;
     const taskStatus = req.params.status;
     try {
-        const result = await client
-            .db('todo-mongo-db')
-            .collection('tasks')
-            .updateOne(
-                { _id: new ObjectId(taskId) },
-                {
-                    $set: { status: taskStatus },
-                }
-            );
-        if (result.modifiedCount === 1) {
-            res.send(`Статус задачи успешно обновлен на "${taskStatus}"`);
-        } else {
-            res.send(`Задача уже имеет статус "${taskStatus}"`);
+        const updatedTask = await Task.findByIdAndUpdate(
+            taskId,
+            {
+                status: taskStatus,
+            },
+            { new: true }
+        );
+
+        if (!updatedTask) {
+            return res.status(404).json({ error: 'Task not found' });
         }
+
+        res.send(updatedTask);
     } catch (error) {
         console.log(error.message);
+        res.send('Не удалось обновить задачу');
     }
 });
 
 app.put('/tasks/:taskId/priority/:priority', async (req, res) => {
+    // изменение приоритета задачи по айди
     const taskId = req.params.taskId;
     const taskPriority = req.params.priority;
     try {
-        const result = await client
-            .db('todo-mongo-db')
-            .collection('tasks')
-            .updateOne(
-                { _id: new ObjectId(taskId) },
-                {
-                    $set: { priority: taskPriority },
-                }
-            );
-        if (result.modifiedCount === 1) {
-            res.send(`Приоритет задачи успешно обновлен на "${taskPriority}"`);
-        } else {
-            res.send(`Задача уже имеет приоритет "${taskPriority}"`);
+        const updatedTask = await Task.findByIdAndUpdate(
+            taskId,
+            {
+                priority: taskPriority,
+            },
+            { new: true }
+        );
+
+        if (!updatedTask) {
+            return res.status(404).json({ error: 'Task not found' });
         }
+
+        res.send(updatedTask);
     } catch (error) {
         console.log(error.message);
+        res.send('Не удалось обновить задачу');
     }
 });
 
 app.delete('/tasks/:taskId', async (req, res) => {
+    // удаление задачи по айди
     const taskId = req.params.taskId;
 
     try {
-        const result = await client
-            .db('todo-mongo-db')
-            .collection('tasks')
-            .deleteOne({ _id: new ObjectId(taskId) });
-        if (result.deletedCount === 1) {
-            res.send(`Задача успешно удалена`);
-        } else {
+        const result = await Task.findByIdAndDelete(taskId);
+        if (!result) {
             res.send(`Задача не найдена`);
+        }
+        if (result) {
+            res.send(`Задача успешно удалена`);
         }
     } catch (error) {
         console.log('Не удалось удалить задачу из MongoDB', error.message);
